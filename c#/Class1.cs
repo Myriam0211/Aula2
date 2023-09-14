38f0b30eddab3e26edf0961f68a0190a374ae591
Myriam0211 <142900549+Myriam0211@users.noreply.github.com>
2023-09-14 11:50:25 -0300

    Add files via upload

---
+++ c#/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generis;
+0referências
+public class Program
+{
+     0referências
+      public static void Main()
+      {
+        List<string> nomes = new List<string>();
+
+       
+        bool continuar = true;
+
+        
+        while (continuar)
+        {
+            Cosole.WriteLine("\nMenu");
+            Console.WriteLine("1. Adicionar o nome");
+            Console.WriteLine("2. AListar nome");
+            console.WriteLine("3. Sair");
+
+            int opcao;
+            if (int.TryParse(Console.ReadLine)
---
+++ c#/Class12.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2;
+class Program
+{
+    static void Main(string[] args)
+    { 
+        Autor tolkien = new Autor
+    {
+        AutorId = 1,
+        Nome = "J.R.R. Tolkien",
+        DataNascimento = new DateTime(1892, 1, 3)
+  };
+
+    //Criar um livro
+    Livro SenhorDosAneis = new Livro
+    {
+        LivroId = 1,
+        Título = "O Senhor dos Anés",
+        AnoPublicacao = 1954,
+        Autor = tolkien
+    };
+
+    //Associar o livro ao autor
+    tolkien.Livros.Add(sennhorDosAneis);
+
+    //Exibir as informações
+    Console.WriteLine($"Autor: {tolkien.Nome)");
+    foreach (var livro in tolkien.Livros)
+    {
+         Console.WriteLine($"Livro: {livro.Título}, Publicado em: {livro.anoPublicacao}");
+    }
+}
+}
